5ebaf01427d0e09ee090cab49fa37903f4e82a7b
Shigasuresh@123 <shigasuresh546@gmail.com>
2020-01-22 22:26:41 +0530

    Initial Commit

---
+++ JSON Swift Codable/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JSON Swift Codable
+//
+//  Created by shiga on 22/01/20.
+//  Copyright © 2020 shiga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
