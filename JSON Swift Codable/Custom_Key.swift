f8ed948ab6f59fa4af78f875317c8e41c1eaa90b
Shigasuresh@123 <suresh.shiga@indigo.co.in>
2020-06-02 12:12:33 +0530

    added

--- JSON Swift Codable/Custom_Key.swift
+++ JSON Swift Codable/Custom_Key.swift
@@ -4,7 +4,7 @@
 //
 //  Created by shiga on 02/06/20.
 //  Copyright © 2020 shiga. All rights reserved.
-//
+//https://medium.com/flawless-app-stories/complete-guide-to-codable-decodable-b1ff696da24f
 
 import Foundation
 
